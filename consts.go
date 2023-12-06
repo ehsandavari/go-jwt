e48b099ccba34c96afb97b0c548a8374d1fae46d
ehsan <ehsan.d@780.ir>
2023-12-06 13:24:05 +0330

    remove consts

--- consts.go
+++
@@ -1,9 +0,0 @@
-package jwt
-
-const (
-	ContextKeyUserID              = "userId"
-	ContextKeyEmail               = "email"
-	ContextKeyEmailVerified       = "emailVerified"
-	ContextKeyPhoneNumber         = "phoneNumber"
-	ContextKeyPhoneNumberVerified = "phoneNumberVerified"
-)
